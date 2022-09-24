0e53555ba40919ef9816bdab9319511005ee4b5c
syoungee <sunyoungah69@gmail.com>
2022-09-24 13:46:53 +0900

    update: dummy data

--- src/dummies.js
+++ src/dummies.js
@@ -1,43 +1,37 @@
 export const DUMMY = [
   {
     id: 1637316103000,
-    title: "제목1 제목1 ",
+    title: '제목1 제목1 ',
     createdAt: 1637316103000,
     likeCount: 111,
-    imageUrl:
-      "https://images.unsplash.com/photo-1635519882252-368d96c38542?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+    imageUrl: 'https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_960_720.jpg',
   },
   {
     id: 1637316104001,
-    title: "제목2 제목2 제목2 제목2 제목2 ",
+    title: '제목2 제목2 제목2 제목2 제목2 ',
     createdAt: 1637316103000,
     likeCount: 222,
-    imageUrl:
-      "https://images.unsplash.com/photo-1637286987126-0ae9e92a0b06?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+    imageUrl: 'https://cdn.pixabay.com/photo/2016/01/08/11/57/butterflies-1127666_960_720.jpg',
   },
   {
     id: 1637316105002,
-    title:
-      "제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3",
+    title: '제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3 제목3',
     createdAt: 1637316103000,
     likeCount: 333,
-    imageUrl:
-      "https://images.unsplash.com/photo-1637183092403-df1a107b01f5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80",
+    imageUrl: 'https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_960_720.jpg',
   },
   {
     id: 1637316106000,
-    title: "제목4",
+    title: '제목4',
     createdAt: 1637316103000,
     likeCount: 444,
-    imageUrl:
-      "https://images.unsplash.com/photo-1637188547317-c83c99a634d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=685&q=80",
+    imageUrl: 'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_960_720.jpg',
   },
   {
     id: 1637316107000,
-    title: "제목5 제목5 제목5제목5 제목5 제목5 제목5 제목5 제목5 제목5",
+    title: '제목5 제목5 제목5제목5 제목5 제목5 제목5 제목5 제목5 제목5',
     createdAt: 1637316103000,
     likeCount: 555,
-    imageUrl:
-      "https://images.unsplash.com/photo-1637189321773-0fe06cdcd1de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    imageUrl: 'https://cdn.pixabay.com/photo/2017/04/09/09/56/avenue-2215317_960_720.jpg',
   },
 ];
