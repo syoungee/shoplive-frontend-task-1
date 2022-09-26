259b0cbac070cb9e07b7fc0e9412c4856ba312ac
syoungee <sunyoungah69@gmail.com>
2022-09-26 20:43:16 +0900

    build: basename 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import EditPage from './pages/EditPage';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/edit/:id" element={<EditPage />} />
