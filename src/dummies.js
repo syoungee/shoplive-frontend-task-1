ad98277f953bce731bf2125f51fc7137855cf650
syoungee <sunyoungah69@gmail.com>
2022-09-28 13:34:12 +0900

    refactor: dummy data

--- src/dummies.js
+++ src/dummies.js
@@ -1,28 +1,29 @@
 export const DUMMY = [
   {
     id: 1637316103000,
-    title: 'strawberry moon\n커다란 strawberry moon 한 스쿱\n나에게 너를 맡겨볼래 eh-oh\n바람을 세로질러 날아오르는 기분 so cool\n삶이 어떻게 더 완벽해 ooh~',
+    title:
+      'strawberry moon \n커다란 strawberry moon 한 스쿱 \n나에게 너를 맡겨볼래 eh-oh \n바람을 세로질러 날아오르는 기분 so cool \n삶이 어떻게 더 완벽해 ooh~',
     createdAt: 1637316103000,
     likeCount: 111,
     imageUrl: 'https://velog.velcdn.com/images/syoungee/post/cff22f96-86ea-4376-8a04-2c329f5ba563/image.JPG',
   },
   {
     id: 1637316104001,
-    title: 'Lilac\n오 라일락 꽃이 지는 날 goodbye\n이런 결말이 어울려\n안녕 꽃잎 같은 안녕\n하얀 우리 봄날의 climax\n아 얼마나 기쁜 일이야',
+    title: 'Lilac \n오 라일락 꽃이 지는 날 goodbye \n이런 결말이 어울려 \n안녕 꽃잎 같은 안녕 \n하얀 우리 봄날의 climax \n아 얼마나 기쁜 일이야',
     createdAt: 1637316103000,
     likeCount: 222,
     imageUrl: 'https://velog.velcdn.com/images/syoungee/post/5667a2a6-e5ed-4ab6-895a-caab5c8744c6/image.PNG',
   },
   {
     id: 1637316105002,
-    title: '9월 17일 예인이와 데이트\n 아이크v3 너무 예쁘잖아🤍\n 오늘만을 기다려왔다고',
+    title: '9월 17일 예인이와 데이트 \n아이크v3 너무 예쁘잖아🤍 \n오늘만을 기다려왔다고',
     createdAt: 1637316103000,
     likeCount: 333,
     imageUrl: 'https://velog.velcdn.com/images/syoungee/post/1f312a69-96a1-4598-b09b-c6c9aeb5f11d/image.JPG',
   },
   {
     id: 1637316106000,
-    title: '하루 끝\n너무 좋아서 그래\n나 시무룩한 얼굴 하고 있지만\n또 기다리다 고민만 하다\n흘러가는 하루 끝에서 하는 말\n내가 널 사랑해',
+    title: '하루 끝 \n너무 좋아서 그래 \n나 시무룩한 얼굴 하고 있지만 \n또 기다리다 고민만 하다 \n흘러가는 하루 끝에서 하는 말 \n내가 널 사랑해',
     createdAt: 1637316103000,
     likeCount: 444,
     imageUrl: 'https://velog.velcdn.com/images/syoungee/post/0e259525-eccb-4700-bd0a-cf09d7ecb1ed/image.JPG',
@@ -30,7 +31,7 @@ export const DUMMY = [
   {
     id: 1637316107000,
     title:
-      'Ah puh\n어어어 푸푸푸 또 🌊\n 허허허 우우우적🌊\n거거거 리더던 시🌊\n저저절 나라면 \n워어언 이 사람아\n언제적 얘길 꺼내나\n보란듯이 헤엄치기 처첨버벙',
+      'Ah puh \n어어어 푸푸푸 또 🌊 \n허허허 우우우적🌊 \n거거거 리더던 시🌊 \n저저절 나라면 \n워어언 이 사람아 \n언제적 얘길 꺼내나 \n보란듯이 헤엄치기 처첨버벙',
     createdAt: 1637316103000,
     likeCount: 555,
     imageUrl: 'https://velog.velcdn.com/images/syoungee/post/7f6769d0-d2e8-4ce6-b066-156e4e829e20/image.JPG',
