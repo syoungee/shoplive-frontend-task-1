aa6944218ec84d5c6979bb87aecd67432bccf7ef
syoungee <sunyoungah69@gmail.com>
2022-09-28 10:13:11 +0900

    add: comments

--- src/components/HighLight.jsx
+++ src/components/HighLight.jsx
@@ -1,3 +1,10 @@
+/*
+  text highlight
+
+  regex expression
+  g = global, match all instances of the pattern in a string, not just one.
+  i = case-insensitive (so, for example, /a/i will match the string "a" or "A".
+*/
 const highLightText = (text, query) => {
   if (query !== '' && text.includes(query)) {
     const parts = text.split(new RegExp(`(${query})`, 'gi'));
